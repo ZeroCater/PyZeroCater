824af8613db0c5f203c0b2f7cebd830ee80eea5d
Brandon Birkholz <brandonjbirkholz@gmail.com>
2015-10-07 11:29:37 -0700

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,21 @@
-from setuptools import setup, find_packages
-
-version = '0.0.1'
+from setuptools import setup
 
 setup(name='zerocater',
-      version=version,
+      version='0.0.1',
       description="Python interface to ZeroCater",
       long_description='',
       keywords='zerocater food delivery meal planning catering lunch',
       author='ZeroCater',
       author_email='tech@zerocater.com',
-      url='https://zerocater.com',
+      url='https://github.com/ZeroCater/PyZeroCater',
+      download_url='https://github.com/ZeroCater/PyZeroCater/tarball/0.0.1',
       license='MIT',
-      packages=find_packages(),
+      packages=['zerocater'],
       classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "Topic :: Internet :: WWW/HTTP",
-      ],
-      include_package_data=True,
-      zip_safe=False
+      ]
   )
